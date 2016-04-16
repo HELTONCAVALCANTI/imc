9e93b125da5e4e98494b7423f401e7ed168e4046
HELTONCAVALCANTI <heltoncavalcanti2010@gmail.com>
2016-04-16 16:53:24 -0300

    Inclusão do arquivo exibe.java

---
+++ src/main/java/exibe.java
@@ -0,0 +1,17 @@
+	
+	public class Exibe {
+	
+	
+	public static void main(string[] args){
+	
+	IMC imc = new IMC();
+	
+	double resultado = imc.CalculaIMC(1.70, 90);
+	
+	System.out.println("O resultado é IMC: " + resultado);
+	
+	
+	}
+	
+	
+	}
